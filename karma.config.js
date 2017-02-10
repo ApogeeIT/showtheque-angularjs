3e68b4e430812fa36bae1b22edc960ee0f63ec08
emmanuel <e.lebray@groupeonepoint.com>
2017-02-10 16:18:42 +0100

    add ut env

---
+++ karma.config.js
@@ -0,0 +1,46 @@
+module.exports = function (config) {
+    'use strict';
+
+    config.set({
+        autoWatch: true,
+        basePath: './',
+        frameworks: [
+            "jasmine"
+        ],
+
+        // list of files / patterns to load in the browser
+        files: [
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/toastr/build/toastr.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/angular/angular.js',
+            'node_modules/angular-route/angular-route.js',
+            "src/**/*.js",
+            "test/**/*.js"
+        ],
+        exclude: [
+        ],
+        port: 8080,
+        browsers: [
+            "PhantomJS"
+        ],
+
+        //reporters: ['progress', 'coverage'],
+        //preprocessors: {
+        //    'app/**/*.js': ['coverage']
+        //},
+        //coverageReporter: {
+        //    type: 'html',
+        //    dir: 'coverage'
+        //},
+
+        plugins: [
+            "karma-phantomjs-launcher",
+            "karma-jasmine",
+            //'karma-coverage' // required for coverage
+        ],
+        singleRun: true,
+        colors: true,
+        logLevel: config.LOG_INFO,
+    });
+};
---
+++ test/env.js
@@ -0,0 +1,5 @@
+describe('unit test env', function(){
+    it('1 to be 1', function(){
+        expect(1).toBe(1);
+    }) 
+}) 
